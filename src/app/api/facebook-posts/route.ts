957a411c1edc322212cd61f6ef7a1eb904537512
Ciprian R <ciprian.rotopanescu@gmail.com>
2025-11-12 19:41:14 +0000

    modified:   src/app/api/facebook-posts/route.ts

--- src/app/api/facebook-posts/route.ts
+++ src/app/api/facebook-posts/route.ts
@@ -39,7 +39,7 @@ export async function GET() {
 
     // Facebook Graph API endpoint to get page posts (sorted by created_time desc)
     const fields = 'id,message,story,created_time,picture,full_picture,permalink_url'
-    const url = `https://graph.facebook.com/v19.0/${pageId}/posts?fields=${fields}&limit=8&access_token=${accessToken}`
+    const url = `https://graph.facebook.com/v19.0/${pageId}/posts?fields=${fields}&limit=10&access_token=${accessToken}`
 
     const response = await fetch(url, {
       headers: {
