d81faeaa53ab2f736970ccdc91f4cd50c1b03283
Ciprian R <ciprian.rotopanescu@gmail.com>
2025-11-14 20:19:42 +0000

    modified:   src/components/Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -61,7 +61,7 @@ export default function Hero() {
         <div 
           className="absolute inset-0" 
           style={{
-            backgroundImage: `url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23ffffff' fill-opacity='0.1'%3E%3Ccircle cx='30' cy='30' r='2'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")"
+            backgroundImage: `url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23ffffff' fill-opacity='0.1'%3E%3Ccircle cx='30' cy='30' r='2'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`
           }} 
         />
       </div>
