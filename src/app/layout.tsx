29803de12ebe7821f43d65a4939c3b8496744918
Ciprian R <ciprian.rotopanescu@gmail.com>
2025-11-17 23:03:54 +0000

    modified:   src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,8 +47,7 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: '/favicon.svg', type: 'image/svg+xml' },
-      { url: '/favicon.ico', sizes: '32x32' },
+      { url: '/favicon.png', type: 'image/png' },
     ],
     apple: '/apple-touch-icon.webp',
   },
