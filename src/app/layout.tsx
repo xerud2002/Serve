97ea62cd75ccecb23d289ad94284c41503f5d23e
Ciprian <ciprian.rotopanescu@gmail.com>
2025-11-21 14:02:09 +0000

    modified:   src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -119,9 +119,11 @@ export default function RootLayout({
   return (
     <html lang="en-GB">
       <head>
-        {/* <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" /> */}
+        {/* Performance optimizations */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link rel="dns-prefetch" href="https://graph.facebook.com" />
         
-      
         {/* PWA Meta Tags */}
         <meta name="application-name" content="SERVE Charity" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
@@ -130,9 +132,6 @@ export default function RootLayout({
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="theme-color" content="#1664de" />
-        
-        {/* Critical resource hints for Facebook API */}
-        <link rel="dns-prefetch" href="https://graph.facebook.com" />
         
         <StructuredData type="local-business" />
       </head>
